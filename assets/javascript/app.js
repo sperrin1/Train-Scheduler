1c8dbaa5b750b8c9b2e97bee9cef69037954ade9
Shawn Perrin <reszka2@gmail.com>
2018-07-20 15:02:31 -0700

    Firebase is undefined...back to the docs :(

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,26 +17,35 @@ var getKey = '';
 
 $(document).ready(function () {
 
-    name = $('#name-input').val().trim();
-    destination = $('#destination-input').val().trim();
-    firstTrainTime = $('#first-train-time-input').val().trim();
-    frequency = $('#frequency-input').val().trim();
-    firstTimeConverted = moment(firstTrainTime, "hh:mm").subtract(1, "years");
-    currentTime = moment();
-    diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-    tRemainder = diffTime % frequency;
-    minutesTillTrain = frequency - tRemainder;
-    nextTrain = moment().add(minutesTillTrain, "minutes");
-    nextTrainFormatted = moment(nextTrain).format("hh:mm");
+    $("#add-train").on("click", function () {
+        name = $('#name-input').val().trim();
+        destination = $('#destination-input').val().trim();
+        firstTrainTime = $('#first-train-time-input').val().trim();
+        frequency = $('#frequency-input').val().trim();
+        firstTimeConverted = moment(firstTrainTime, "hh:mm").subtract(1, "years");
+        currentTime = moment();
+        diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+        tRemainder = diffTime % frequency;
+        minutesTillTrain = frequency - tRemainder;
+        nextTrain = moment().add(minutesTillTrain, "minutes");
+        nextTrainFormatted = moment(nextTrain).format("hh:mm");
 
-    // Code for the push
-    keyHolder = dataRef.push({
-        name: name,
-        destination: destination,
-        firstTrainTime: firstTrainTime,
-        frequency: frequency,
-        nextTrainFormatted: nextTrainFormatted,
-        minutesTillTrain: minutesTillTrain
+        // Code for the push
+        keyHolder = dataRef.push({
+            name: name,
+            destination: destination,
+            firstTrainTime: firstTrainTime,
+            frequency: frequency,
+            nextTrainFormatted: nextTrainFormatted,
+            minutesTillTrain: minutesTillTrain
+        });
+
+        $('#name-input').val('');
+        $('#destination-input').val('');
+        $('#first-train-time-input').val('');
+        $('#frequency-input').val('');
+
+        return false;
     });
 
 });
