bcc320cb00cb8ed5a891a842be382aa589207a24
Shawn Perrin <reszka2@gmail.com>
2018-07-24 11:20:43 -0700

    still can't get it to push to firebase...que la chinga

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -26,37 +26,43 @@ var config = {
 };
 firebase.initializeApp(config);
 
+var database = firebase.database();
+
 $(document).ready(function () {
 
-    $("#add-train").on("click", function () {
-        name = $('#name-input').val().trim();
-        destination = $('#destination-input').val().trim();
-        firstTrainTime = $('#first-train-time-input').val().trim();
-        frequency = $('#frequency-input').val().trim();
-        firstTimeConverted = moment(firstTrainTime, "hh:mm").subtract(1, "years");
-        currentTime = moment();
-        diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-        tRemainder = diffTime % frequency;
-        minutesTillTrain = frequency - tRemainder;
-        nextTrain = moment().add(minutesTillTrain, "minutes");
-        nextTrainFormatted = moment(nextTrain).format("hh:mm");
+    database.ref("/train-sheduler").on("child_added", function (snapshot) {
 
-        // Code for the push
-        keyHolder = dataRef.push({
-            name: name,
-            destination: destination,
-            firstTrainTime: firstTrainTime,
-            frequency: frequency,
-            nextTrainFormatted: nextTrainFormatted,
-            minutesTillTrain: minutesTillTrain
+        $("#add-train").on("click", function () {
+            name = $('#name-input').val().trim();
+            destination = $('#destination-input').val().trim();
+            firstTrainTime = $('#first-train-time-input').val().trim();
+            frequency = $('#frequency-input').val().trim();
+            firstTimeConverted = moment(firstTrainTime, "hh:mm").subtract(1, "years");
+            currentTime = moment();
+            diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+            tRemainder = diffTime % frequency;
+            minutesTillTrain = frequency - tRemainder;
+            nextTrain = moment().add(minutesTillTrain, "minutes");
+            nextTrainFormatted = moment(nextTrain).format("hh:mm");
+
+            // Code for the push
+            keyHolder = dataRef.push({
+                name: name,
+                destination: destination,
+                firstTrainTime: firstTrainTime,
+                frequency: frequency,
+                nextTrainFormatted: nextTrainFormatted,
+                minutesTillTrain: minutesTillTrain
+            });
+
+            $('#name-input').val('');
+            $('#destination-input').val('');
+            $('#first-train-time-input').val('');
+            $('#frequency-input').val('');
+
+            return false;
         });
 
-        $('#name-input').val('');
-        $('#destination-input').val('');
-        $('#first-train-time-input').val('');
-        $('#frequency-input').val('');
-
-        return false;
     });
 
 });
